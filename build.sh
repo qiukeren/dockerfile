2505e13f33ab762bff8f0abc35d2568c7781c46e
qiuker521 <qiuker521@163.com>
2017-05-22 15:10:49 +0800

    add php5

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
+docker pull alpine:latest
 cd base
 docker build --tag paas_base .
 cd ..
-cd php
-docker build --tag paas_php .
+cd php7
+docker build --tag paas_php7 .
+cd ..
+cd php5
+docker build --tag paas_php5 .
+cd ..
+cd gogs
+docker build --tag paas_gogs .
 cd ..
 docker rmi -f `docker images |grep "<none>"|awk '{print $3}'`
