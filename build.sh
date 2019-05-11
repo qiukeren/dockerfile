89314235766f73bba7b7944fd144515f9ada79f0
datiebangzi <datiebangzi@hotmail.com>
2019-05-11 23:54:24 +0800

    remove php5 && move to openrc

--- build.sh
+++ build.sh
@@ -2,30 +2,19 @@ if [ ! -f "id.rsa.pub" ];then
 	echo "[INFO] public key does not exist, generating"
 	ssh-keygen -t rsa -P '' -f id.rsa
 fi
-cp id.rsa.pub ssh/id_rsa.paas.pub
+cp id.rsa.pub base/id_rsa.paas.pub
 docker pull alpine:latest
 cd base
 docker build --tag paas_base .
 cd ..
-cd ssh
-docker build --tag paas_ssh .
-cd ..
-cd openresty
-docker build --tag paas_openresty .
-cd ..
+
 cd python2
 docker build --tag paas_python2 .
 cd ..
 cd php7
 docker build --tag paas_php7 .
 cd ..
-cd php5
-docker build --tag paas_php5 .
-cd ..
 cd java
 docker build --tag paas_jar .
-cd ..
-cd gogs
-#docker build --tag paas_gogs .
 cd ..
 docker rmi -f `docker images |grep "<none>"|awk '{print $3}'`
